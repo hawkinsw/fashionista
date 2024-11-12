287f6653bd1e0f43fdf7d023bb48e8ce7927125d
Will Hawkins <hawkinsw@obs.cr>
2024-11-11 23:55:27 -0500

    Switch to API.

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -3,13 +3,20 @@ import './App.css';
 import { ColorResult, SketchPicker } from 'react-color';
 
 function App() {
+
   const topObj = useRef<HTMLDivElement>(null)
   const middleObj = useRef<HTMLDivElement>(null)
   const bottomObj = useRef<HTMLDivElement>(null)
   const pickerObj = useRef<HTMLDivElement>(null)
   const sketchPickerObj = useRef<SketchPicker>(null)
+  const apiConfig = useRef<HTMLInputElement>(null)
+
+  const fashionistaDefaultApiUrl = "https://be-autumn-fire-7404.fly.dev/"
 
   const [chosenColor, setChosenColor] = useState<string>("#4A90E2")
+  const [editableChosenApi, setEditableChosenApi] = useState<string>(fashionistaDefaultApiUrl)
+  const [chosenApi, setChosenApi] = useState<string>(fashionistaDefaultApiUrl)
+  const [settleTimer, setSettleTimer] = useState<number>(-1)
 
   useEffect(() => {
     let s = sketchPickerObj!.current!.state as any
@@ -23,79 +30,71 @@ function App() {
     userChoseColor(beginningColor, Object())
   }, [])
 
-  let calculateNewAngle = (angle: number, delta: number): number => {
-    angle += delta;
-    angle %= 360;
-    if (angle < 0) {
-      angle += 360;
+  useEffect(() => {
+    console.log("The API was actually updated!")
+    setSettleTimer(-1)
+  }, [chosenApi])
+
+  useEffect(() => {
+    if (settleTimer != -1) {
+      console.log("Clearing existing settle timer.")
+      window.clearTimeout(settleTimer)
+      setSettleTimer(-1)
     }
-    return angle;
-  }
+    setSettleTimer(window.setTimeout((maybeGoodApiUrl: string) => {
+      try {
+        const url = new URL(maybeGoodApiUrl)
+        setChosenApi(maybeGoodApiUrl)
+      } catch (error) {
+        alert(`Could not update the Fashionista URL: ${error}`)
+      }
+    }, 5000, editableChosenApi))
 
-  let calculateCompliment = (angle: number): number => {
-    console.log(`angle: ${angle}`)
-    angle = calculateNewAngle(angle, 180)
-    console.log(`angle: ${angle}`)
-    return angle
-  }
+    console.log(`Setting the settle timer: ${settleTimer}`)
+  }, [editableChosenApi])
 
-  let calculateAnalogousUpper = (angle: number): number => {
-    console.log(`angle: ${angle}`)
-    angle = calculateNewAngle(angle, 30)
-    console.log(`angle: ${angle}`)
-    return angle
-  }
+  let userChoseColor = async (color: ColorResult, event: ChangeEvent) => {
+    let body = JSON.stringify(color.hsl)
+    let url = new URL(chosenApi)
+    console.log(`body: ${body}`)
 
-  let calculateAnalogousLower = (angle: number): number => {
-    console.log(`angle: ${angle}`)
-    angle = calculateNewAngle(angle, -30)
-    console.log(`angle: ${angle}`)
-    return angle
-  }
+    await fetch(url, {
+      method: "POST", headers: {
+        "Content-Type": "application/json",
+      }, body: `${body}`
+    }).then((response: Response) => {
 
-  let userChoseColor = (color: ColorResult, event: ChangeEvent) => {
-    // Good debugging tool: https://www.sessions.edu/color-calculator/
-
-    /*
-    console.log("hsl: " + color.hsl.h)
-    console.log("hsl: " + color.hsl.s)
-    console.log("hsl: " + color.hsl.l)
-    */
-
-    let complimentaryColor = structuredClone(color.hsl)
-    complimentaryColor.h = calculateCompliment(complimentaryColor.h)
-    let complimentaryColorS = `hsl(${complimentaryColor.h} ${complimentaryColor.s * 100}% ${complimentaryColor.l * 100}%)`
-    /*
-    console.log(complimentaryColorS)
-    */
-
-    let analogousColorUpper = structuredClone(color.hsl)
-    analogousColorUpper.h = calculateAnalogousUpper(analogousColorUpper.h)
-    let analogousColorUpperS = `hsl(${analogousColorUpper.h} ${analogousColorUpper.s * 100}% ${analogousColorUpper.l * 100}%)`
-    /*
-    console.log(analogousColorUpperS)
-    */
-
-    let analogousColorLower = structuredClone(color.hsl)
-    analogousColorLower.h = calculateAnalogousLower(analogousColorLower.h)
-    let analogousColorLowerS = `hsl(${analogousColorLower.h} ${analogousColorLower.s * 100}% ${analogousColorLower.l * 100}%)`
-    /*
-    console.log(analogousColorLowerS)
-    */
-
-    topObj!.current!.style.background = complimentaryColorS
-    middleObj!.current!.style.background = analogousColorUpperS
-    bottomObj!.current!.style.background = analogousColorLowerS
-    pickerObj!.current!.style.background = color.hex
-
-    setChosenColor(color.hex)
+      let body = response.json().then((response: any) => {
+        topObj!.current!.style.background = response.complimentary
+        middleObj!.current!.style.background = response.analogousUpper
+        bottomObj!.current!.style.background = response.analogousLower
+        pickerObj!.current!.style.background = color.hex
+      }, (failure: any) => {
+        console.log(`failure: ${failure}`)
+        topObj!.current!.style.background = 'red'
+        middleObj!.current!.style.background = 'green'
+        bottomObj!.current!.style.background = 'blue'
+        pickerObj!.current!.style.background = color.hex
+      })
+    }, (failure: any) => {
+      console.log(`failure: ${failure}`)
+      topObj!.current!.style.background = 'red'
+      middleObj!.current!.style.background = 'green'
+      bottomObj!.current!.style.background = 'blue'
+      pickerObj!.current!.style.background = color.hex
+    })
   }
 
   return (
     <div className="App">
       <div className="Horizontal">
-        <div ref={pickerObj} className="ColorPicker">
-          <SketchPicker ref={sketchPickerObj} color={chosenColor} onChangeComplete={userChoseColor}/>
+        <div className='FashionistaConfig'>
+          <div ref={apiConfig} className="APIConfig">
+            API Url: <input value={editableChosenApi} onChange={e => setEditableChosenApi(e.target.value)}></input>
+          </div>
+          <div ref={pickerObj} className="ColorPicker">
+            <SketchPicker ref={sketchPickerObj} color={chosenColor} onChangeComplete={userChoseColor} />
+          </div>
         </div>
         <section className="ColorDisplayer">
           <div ref={topObj} className='Top'>
